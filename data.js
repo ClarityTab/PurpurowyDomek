aceaba76c9f356f9e023386c40474b32ad13b9c6
ClarityTab <callitaplan.planit@gmail.com>
2025-11-15 13:59:58 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,263 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-wejscie",
+      "name": "wejscie",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 3.06248555246904,
+        "pitch": -0.031852092768481555,
+        "fov": 1.308072473761003
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6277156371081531,
+          "pitch": 0.12708025099471776,
+          "rotation": 0,
+          "target": "1-salon"
+        },
+        {
+          "yaw": 2.6783436303962036,
+          "pitch": -0.021504578073574976,
+          "rotation": 0,
+          "target": "2-lazienka"
+        },
+        {
+          "yaw": 1.170314688459186,
+          "pitch": 0.017207543536159164,
+          "rotation": 0,
+          "target": "4-taras"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-salon",
+      "name": "salon",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -0.517130392360329,
+        "pitch": 0.015892118536566358,
+        "fov": 1.308072473761003
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4819604993256483,
+          "pitch": 0.04576792474065172,
+          "rotation": 0,
+          "target": "3-antresola"
+        },
+        {
+          "yaw": -0.4513284367102628,
+          "pitch": 0.023735207554693716,
+          "rotation": 0,
+          "target": "4-taras"
+        },
+        {
+          "yaw": -2.641640060286697,
+          "pitch": 0.10320152595775411,
+          "rotation": 0,
+          "target": "0-wejscie"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-lazienka",
+      "name": "lazienka",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 3.103610229089509,
+        "pitch": -0.0629407257249106,
+        "fov": 1.308072473761003
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6201345810695109,
+          "pitch": 0.04078654912646229,
+          "rotation": 0,
+          "target": "0-wejscie"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-antresola",
+      "name": "antresola",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 1.8952302033736448,
+        "pitch": 0.41085672953627395,
+        "fov": 1.308072473761003
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7075528924542063,
+          "pitch": 0.8262239174411548,
+          "rotation": 0,
+          "target": "1-salon"
+        },
+        {
+          "yaw": -2.0779257970771674,
+          "pitch": 0.5116471285906385,
+          "rotation": 0,
+          "target": "4-taras"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-taras",
+      "name": "taras",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -0.32816611033305776,
+        "pitch": 0.018444988886116676,
+        "fov": 1.308072473761003
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1257417157612757,
+          "pitch": -0.4112430618735985,
+          "rotation": 0,
+          "target": "3-antresola"
+        },
+        {
+          "yaw": -2.8733457918622953,
+          "pitch": 0.03138095881979197,
+          "rotation": 0,
+          "target": "0-wejscie"
+        },
+        {
+          "yaw": 2.9611458770579713,
+          "pitch": 0.296300551361389,
+          "rotation": 0,
+          "target": "1-salon"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
